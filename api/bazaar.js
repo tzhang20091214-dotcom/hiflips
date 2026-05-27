944f99f142b974fb65d7221f95628b1274c821a7
tzhang20091214-dotcom <tzhang20091214@gmail.com>
2026-05-27 23:43:24 +0100

    Update bazaar.js

--- api/bazaar.js
+++ api/bazaar.js
@@ -1,9 +1,14 @@
-
 export default async function handler(req, res) {
   try {
-    const r = await fetch("https://api.hypixel.net/v2/skyblock/bazaar");
+    const key = process.env.HYPIXEL_KEY;
+
+    const r = await fetch(
+      `https://api.hypixel.net/v2/skyblock/bazaar?key=${key}`
+    );
+
     const data = await r.json();
     res.status(200).json(data);
+
   } catch (e) {
     res.status(500).json({ error: "Failed to fetch bazaar" });
   }
